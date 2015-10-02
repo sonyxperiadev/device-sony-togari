d27b45b1088b0ef615874e464c02934eebc0768d
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-10-02 08:16:44 +0200

    fix product number
    switch to Global C6833 device
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_c6803.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_c6833.mk
